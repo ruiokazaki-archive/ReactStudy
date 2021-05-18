b6dc5069a13b5f108aabd7f3fb5e86f86ae55910
RuiOkazaki <okazaki200226@gmail.com>
2021-05-18 22:57:09 +0900

    stateとライフサイクル

--- 05/src/index.js
+++ 05/src/index.js
@@ -1,8 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Test(props) {
-  return <h1>{props.text}</h1>;
+// function Test(props) {
+//   return <h1>{props.text}</h1>;
+// }
+
+// function tick() {
+//   const App = <p>{new Date().toLocaleTimeString()}</p>;
+//   ReactDOM.render(App, document.getElementById("root"));
+// }
+
+// setInterval(tick, 1000);
+
+// function Clock(props) {
+//   return <p>{props.date.toLocaleTimeString()}</p>;
+// }
+// function tick() {
+//   ReactDOM.render(<Clock />, document.getElementById("root"));
+// }
+
+function FormatedDate(props) {
+  return <p>{props.date.toLocaleTimeString()}</p>;
 }
 
-ReactDOM.render(<Test text="test" />, document.getElementById("root"));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return <FormatedDate date={this.state.date} />;
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

78e05798a0d5cc91e1d8b133c80c5f90deb56a69
RuiOkazaki <okazaki200226@gmail.com>
2021-05-18 21:07:17 +0900

    コンポーネントとprops

--- src/index.js
+++ src/index.js
@@ -26,14 +26,65 @@ import "./index.css";
 // const element = <h1>Hello, {formatName(name)}!</h1>;
 // ReactDOM.render(getGreeting("ruru"), document.getElementById("root"));
 
-function tick() {
-  const element = (
-    <div>
-      <h1>what time is it</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>what time is it</h1>
+//       <h2>{new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById("root"));
+// }
+
+// setInterval(tick, 1000);
+
+// function Welcome(props) {
+//   return <h1>{props.name}</h1>;
+// }
+// コンポーネント名は常に大文字で始める。
+// const element = <Welcome name="ruru" />;
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="morikawa"></Welcome>
+//       <Welcome name="okazaki"></Welcome>
+//       <Welcome name="tanaka"></Welcome>
+//       <Welcome name="kobayashi"></Welcome>
+//       <Welcome name="wakitani"></Welcome>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return <Comment />;
+// }
+// function Avatar(props) {
+//   <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />;
+// }
+// function UserInfo(props) {
+//   <div className="UserInfo">
+//     <Avatar user={props.user} />
+//     <div className="UserInfo-name">{props.user.name}</div>
+//   </div>;
+// }
+// function Comment(props) {
+//   <div className="Comment">
+//     <UserInfo />
+//     <div className="Comment-text">{props.text}</div>
+//     <div className="Comment-date">{formatDate(props.date)}</div>
+//   </div>;
+// }
+
+// function Sum(a, b) {
+//   return <p>{a + b}</p>;
+// }
+
+function Sum(props) {
+  return <p>{props.numbers.a + props.numbers.b}</p>;
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Sum numbers={{ a: 30, b: 49 }} />,
+  document.getElementById("root")
+);
